1cc12cb3b5dc308e08246308e24be8d776e2e0a4
Kevin Thierauf <thieraufk@wit.edu>
2021-03-03 13:22:53 -0500

    Initial Commit

---
+++ TextBox.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Computer on 3/3/2021.
+//
+
+#include "TextBox.hpp"
+
+namespace sftb {
+}
---
+++ TextBox.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Computer on 3/3/2021.
+//
+
+#ifndef SFML_TEXTBOX_TEXTBOX_HPP
+#define SFML_TEXTBOX_TEXTBOX_HPP
+
+namespace sftb {
+    class TextBox {
+    };
+}
+
+
+#endif //SFML_TEXTBOX_TEXTBOX_HPP
